4ab839137c28abda08f4cd584591e301b33d1ce9
Zeeshan Yousuf <manoswati@gmail.com>
2017-10-08 18:54:22 -0700

    created Index.php as a landing page for home owner to fill the form.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Bold Leads</title>
+</head>
+<body>
+
+    <form action="#_" method="post">
+        First Name : <input type="type" name="first_name" placeholder="First Name"> 
+        <br />
+        Last Name : <input type="type" name="last_name" placeholder="Last Name"> 
+        <br />
+        Phone : <input type="type" name="phone" placeholder="Phone"> 
+        <br />
+        Email : <input type="type" name="email" placeholder="Email"> 
+        <br />
+        Address : <input type="type" name="address" placeholder="Address"> 
+        <br />
+        Home SQFT : <input type="type" name="home_square_footage" placeholder="Home Square Footage"> 
+        <br />
+        <button type="submit">Submit</button>
+    </form>    
+</body>
+</html>                             
