0aa9f4383c5441ff7a4b75e2fcce8908e40e38cd
Zeeshan Yousuf <manoswati@gmail.com>
2017-10-08 19:31:56 -0700

    updated UI of form in index.php, added bootstrap lib for UI  and created style.css for custom UI design

--- index.php
+++ index.php
@@ -5,23 +5,70 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Bold Leads</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
+    <div class="container">
+        <div class="col-md-12 title-top">
+            <h1 class="title-top text-center">Do you want to sell your House?</h1>
+            <h3 class="title-top text-center">Find Out What Your House is Worth</h3>
+        </div>
 
-    <form action="#_" method="post">
-        First Name : <input type="type" name="first_name" placeholder="First Name"> 
-        <br />
-        Last Name : <input type="type" name="last_name" placeholder="Last Name"> 
-        <br />
-        Phone : <input type="type" name="phone" placeholder="Phone"> 
-        <br />
-        Email : <input type="type" name="email" placeholder="Email"> 
-        <br />
-        Address : <input type="type" name="address" placeholder="Address"> 
-        <br />
-        Home SQFT : <input type="type" name="home_square_footage" placeholder="Home Square Footage"> 
-        <br />
-        <button type="submit">Submit</button>
-    </form>    
+        <div class="col-md-8 form-adj">
+            <div class="form-main">
+                <div class="form-header">
+                    <h4>There is No Harm in Talking with us</h4>
+                </div>
+                <div class="form-inner">
+                    <form class="form-horizontal" action="#_" method="post">
+                        <div class="form-group">
+                            <label for="first_name" class="col-lg-3">First Name</label>
+                            <div class="col-lg-9">
+                                <input type="text" class="form-control" name="first_name" placeholder="First Name" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="last_name" class="col-lg-3">Last Name</label>
+                            <div class="col-lg-9">
+                                <input type="text" class="form-control" name="last_name" placeholder="Last Name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="phone" class="col-lg-3">Phone</label>
+                            <div class="col-lg-9">
+                                <input type="phone" class="form-control" name="phone" placeholder="Phone" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="email" class="col-lg-3">Email</label>
+                            <div class="col-lg-9">
+                                <input type="email" class="form-control" name="email" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="address" class="col-lg-3">Address</label>
+                            <div class="col-lg-9">
+                                <input type="text" class="form-control" name="address" placeholder="Address">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="home_square_footage" class="col-lg-3">Home sqft</label>
+                            <div class="col-lg-9">
+                                <input type="text" class="form-control" name="home_square_footage" placeholder="Home square footage">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-lg-9"></div>
+                            <div class="col-lg-3">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>                             
